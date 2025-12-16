c29a653cee585d86b19cff8eadd5c44d96a88326
Karol <karolostapinski9@gmail.com>
2025-12-16 12:25:59 +0100

    Pliki do wezlow

---
+++ NetSim/include/nodes.hxx
@@ -0,0 +1,8 @@
+//
+// Created by Karol on 16/12/2025.
+//
+
+#ifndef NETSIM_NODES_HXX
+#define NETSIM_NODES_HXX
+
+#endif //NETSIM_NODES_HXX
---
+++ NetSim/src/nodes.cpp
@@ -0,0 +1,3 @@
+//
+// Created by Karol on 16/12/2025.
+//
