6d2534e8d776a2b0c5383dab4f88f6c0e759cb35
Magin28 <marcin.ostachowicz@gmail.com>
2026-01-08 13:56:31 +0100

    nowe testy

--- NetSim/tests/tests.cpp
+++ NetSim/tests/tests.cpp
@@ -1,14 +1,41 @@
 #include <gtest/gtest.h>
+#include "gmock/gmock.h"
 #include "package.hxx"
 #include "storage_types.hxx"
 #include <utility>
 #include "nodes.hxx"
+#include <gtest.h>
 
+// main mocks
 
-TEST(WorkerTest, HasBuffer) {
-    // Test scenariusza opisanego na stronie:
-    // http://home.agh.edu.pl/~mdig/dokuwiki/doku.php?id=teaching:programming:soft-dev:topics:net-simulation:part_nodes#bufor_aktualnie_przetwarzanego_polproduktu
+#define NODES_MOCKS_HPP_
 
+class MockReceiver : public IPackageReceiver
+{
+public:
+    MOCK_METHOD1(receive_package, void(Package &&));
+
+    MOCK_CONST_METHOD0(begin, IPackageStockpile::const_iterator());
+
+    MOCK_CONST_METHOD0(cbegin, IPackageStockpile::const_iterator());
+
+    MOCK_CONST_METHOD0(end, IPackageStockpile::const_iterator());
+
+    MOCK_CONST_METHOD0(cend, IPackageStockpile::const_iterator());
+
+#ifdef WITH_RECEIVER_TYPE
+    MOCK_CONST_METHOD0(get_receiver_type, ReceiverType());
+#endif
+
+    MOCK_CONST_METHOD0(get_id, ElementID());
+
+    MOCK_CONST_METHOD0(get_delivery_interval, TimeOffset());
+};
+
+// TEST połprodukty
+
+TEST(WorkerTest, HasBuffer)
+{
     Worker w(1, 2, std::make_unique<PackageQueue>(PackageQueueType::FIFO));
     Time t = 1;
 
@@ -17,15 +44,14 @@ TEST(WorkerTest, HasBuffer) {
     ++t;
     w.receive_package(Package(2));
     w.do_work(t);
-    auto& buffer = w.get_sending_buffer();
+    auto &buffer = w.get_sending_buffer();
 
     ASSERT_TRUE(buffer.has_value());
     EXPECT_EQ(buffer.value().get_id(), 1);
 }
 
-TEST(PackageTest, IsAssignedIdLowest) {
-    // przydzielanie ID o jeden większych -- utworzenie dwóch obiektów pod rząd
-
+TEST(PackageTest, IsAssignedIdLowest)
+{
     Package p1;
     Package p2;
 
@@ -33,9 +59,8 @@ TEST(PackageTest, IsAssignedIdLowest) {
     EXPECT_EQ(p2.get_id(), 2);
 }
 
-TEST(PackageTest, IsIdReused) {
-    // przydzielanie ID po zwolnionym obiekcie
-
+TEST(PackageTest, IsIdReused)
+{
     {
         Package p1;
     }
@@ -44,21 +69,24 @@ TEST(PackageTest, IsIdReused) {
     EXPECT_EQ(p2.get_id(), 1);
 }
 
-TEST(PackageTest, IsMoveConstructorCorrect) {
+TEST(PackageTest, IsMoveConstructorCorrect)
+{
     Package p1;
     Package p2(std::move(p1));
 
     EXPECT_EQ(p2.get_id(), 1);
 }
 
-TEST(PackageTest, IsAssignmentOperatorCorrect) {
+TEST(PackageTest, IsAssignmentOperatorCorrect)
+{
     Package p1;
     Package p2 = std::move(p1);
 
     EXPECT_EQ(p2.get_id(), 1);
 }
 
-TEST(PackageQueueTest, IsFifoCorrect) {
+TEST(PackageQueueTest, IsFifoCorrect)
+{
     PackageQueue q(PackageQueueType::FIFO);
     q.push(Package(1));
     q.push(Package(2));
@@ -70,7 +98,8 @@ TEST(PackageQueueTest, IsFifoCorrect) {
     EXPECT_EQ(p.get_id(), 2);
 }
 
-TEST(PackageQueueTest, IsLifoCorrect) {
+TEST(PackageQueueTest, IsLifoCorrect)
+{
     PackageQueue q(PackageQueueType::LIFO);
     q.push(Package(1));
     q.push(Package(2));
@@ -80,4 +109,25 @@ TEST(PackageQueueTest, IsLifoCorrect) {
 
     p = q.pop();
     EXPECT_EQ(p.get_id(), 1);
+}
+
+// TEST węzły sieci
+
+TEST(RampTest, IsDeliveryOnTime)
+{
+
+    Ramp r(1, 2);
+    auto recv = std::make_unique<Storehouse>(1);
+
+    r.receiver_preferences_.add_receiver(recv.get());
+
+    r.deliver_goods(1);
+    ASSERT_TRUE(r.get_sending_buffer().has_value());
+    r.send_package();
+
+    r.deliver_goods(2);
+    ASSERT_FALSE(r.get_sending_buffer().has_value());
+
+    r.deliver_goods(3);
+    ASSERT_TRUE(r.get_sending_buffer().has_value());
 }
