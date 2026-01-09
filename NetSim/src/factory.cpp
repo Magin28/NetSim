105905c6d4e0fd32f00d31e669ac31c5f880e8aa
Karol <karolostapinski9@gmail.com>
2026-01-09 11:09:06 +0100

    Poprawione cpp

--- NetSim/src/factory.cpp
+++ NetSim/src/factory.cpp
@@ -115,22 +115,11 @@ std::vector<std::string> character_split(const std::string& splittable_str, char
 }
 
 ParsedLineData parse_line(std::string& line) {
-    // rozłóż linię na ID określający typ elementu oraz tokeny "klucz=wartość"
-    // każdy token rozłóż na parę (klucz, wartość)
-    // na podstawie ID oraz par (klucz, wartość) zwróć odpowiedni obiekt typu ParsedLineData
     std::vector<std::string> tokens;
     std::string token;
 
     std::istringstream token_stream(line);
     char delimiter = ' ';
-
-    //  Schemat wstępnego przetwarzania pojedynczej linii (rodzaju analizy składniowej – sprawdzenia zgodności z gramatyką języka) jest identyczny dla każdego typu elementu sieci (dla każdego typu węzła i dla połączenia)
-    //    sprawdź, czy linia zaczyna się od jednego z predefiniowanych znaczników (np. LINK); jeśli nie – rzuć wyjątek
-    //    wyodrębnij występujące po znaczniku pary klucz-wartość (w postaci “klucz=wartość”, rozdzielone spacjami)
-    //    i zachowaj je w kontenerze umożliwiającym ich wygodną analizę (np. w postaci mapy);
-    //    zwróć uwagę, że na tym etapie wszystkie dane występujące pomiędzy znakiem “=” a spacją powinny być potraktowane jako wartość
-    //    – zatem wartość skojarzona z kluczem będzie łańcuchem znaków (np. typu std::string)
-
 
     while (std::getline(token_stream, token, delimiter))
         tokens.push_back(token);
@@ -218,15 +207,6 @@ void link(Factory& factory, const std::map<std::string, std::string>& parameters
 }
 
 Factory load_factory_structure(std::istream& is) {
-    // utwórz (pusty) obiekt typu Factory
-    //
-    // dla każdej linii w pliku
-    //      jeśli linia pusta lub rozpoczyna się od znaku komentarza - przejdź do kolejnej linii
-    //      dokonaj parsowania linii
-    //      w zależności od typu elementu - wykorzystaj pary (klucz, wartość) do poprawnego:
-    //       * zainicjalizowania obiektu właściwego typu węzła i dodania go do obiektu fabryki, albo
-    //       * utworzenia połączenia między zadanymi węzłami sieci
-
     Factory factory;
 
     std::string line;
@@ -298,8 +278,6 @@ void link_stream_fill(std::stringstream& link_stream, const PackageSender& packa
 }
 
 void save_factory_structure(Factory& factory, std::ostream& os) {
-    // Elementy w pliku występującą w następującej kolejności:
-    // LOADING_RAMP, WORKER, STOREHOUSE, LINK.
 
     std::stringstream link_stream;
 
