43238cdb3daf727249033e2f8b821de2128c9b37
goo1iat <stasiek4023@gmail.com>
2026-01-12 22:28:13 +0100

    poprawa

--- NetSim/main.cpp
+++ NetSim/main.cpp
@@ -3,6 +3,39 @@
 #include "types.hxx"
 #include "storage_types.hxx"
 #include "gtest/gtest.h"
+<<<<<<< Updated upstream
+=======
+#include "simulation.hxx"
+#include "raports.hxx"
+
+int main() {
+    const std::string project_root = "../";
+    const std::string filename = project_root + "dane/dane.txt";
+    const std::string output_dir = project_root + "Wyniki";
+    const std::string struct_out_file = output_dir + "/struct-report.txt";
+    const std::string sim_out_file = output_dir + "/sim-report.txt";
+    const TimeOffset simulation_steps = 30;
+    std::cout << "--- START: NetSim ---" << std::endl;
+
+    std::ifstream input_file(filename);
+    Factory factory;
+
+    factory = load_factory_structure(input_file);
+    std::ofstream struct_os(struct_out_file);
+    struct_os.is_open();
+    generate_structure_report(factory, struct_os);
+    struct_os.close();
+
+    std::ofstream sim_os(sim_out_file);
+    simulate(factory, simulation_steps, [&sim_os](Factory& f, Time t) {
+        generate_simulation_turn_report(f, sim_os, t);
+    });
+    sim_os.close();
+    std::cout << "--- KONIEC: Symulacja zakonczona sukcesem ---" << std::endl;
+
+    return 0;
+}
+>>>>>>> Stashed changes
 
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
